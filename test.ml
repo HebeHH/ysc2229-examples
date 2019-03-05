a1670dcb11f01af0a293646a90e30a7b97d78cad
Berke Utku Onder <40127290+berkeutkuonder@users.noreply.github.com>
2019-03-05 19:37:34 +0800

    Add files via upload

--- test.ml
+++ test.ml
@@ -1,79 +1,14 @@
+(*Library Functions *)
 let swap arr i j =
   let tmp = arr.(i) in
   arr.(i) <- arr.(j);
   arr.(j) <- tmp;;
-
 
 let fact n =
   let rec helper n acc =
     if n = 0 then acc
     else helper (n-1) (acc*n) in
   helper n 1;;
-
-let print_int_sub_array l u arr =
-  assert (l <= u);
-  assert (u <= Array.length arr);
-  Printf.printf "[| ";
-  for i = l to u - 1 do
-    Printf.printf "%d" arr.(i);
-    if i < u - 1
-    then Printf.printf "; "
-    else ()
-  done;
-  Printf.printf " |] "
-
-let print_int_array arr =
-  let len = Array.length arr in
-  print_int_sub_array 0 (len) arr
-
-let equivalent ls1 ls2 =
-  let contains_all xs1 xs2 =
-    List.for_all (fun e -> List.exists (fun x -> x = e) xs2) xs1
-  in
-  (List.length ls1 == List.length ls2) &&
-    (contains_all ls1 ls2) &&
-      (contains_all ls2 ls1)
-  ;;
-
-
-(* Exercise 7.2 1: *)
-(* Very space intensive - copies arrays way too much - should be a way to not do that*)
-
-let perm_unit_test (cand: 'a array -> 'a array list) =
-  cand [||] = [[||]]  &&
-    equivalent (cand [|1; 2; 3|])
-      [[|1; 2; 3|]; [|1; 3; 2|]; [|2; 1; 3|]; [|2; 3; 1|]; [|3; 1; 2|]; [|3; 2; 1|]]  &&
-      equivalent (cand [|1;2;3;4|]) (cand [|3;4;2;1|])
-;;
-
-
-let permutations a =
-  let len = Array.length(a) in
-  let rec perms arr i =
-    if i = len then [Array.copy a]
-    else
-      let ls = ref [] in
-      for j = i to len - 1 do
-        swap arr i j;
-        let sub_perms = perms arr (i + 1) in
-        List.iter (fun sp -> sp.(i) <- arr.(i)) sub_perms;
-        swap arr i j;
-        ls := !ls @ sub_perms
-      done;
-      !ls
-  in perms (Array.copy a) 0;;
-
-(* 
-Passes the unit test; also passes the polymorphic unit test, done separately bc ocaml
-can be a bit too typesafe at times.
-*)
-perm_unit_test permutations;;
-equivalent (permutations [|(1,'a');(2,'b');(3,'c');(4,'d')|])
-  (permutations [|(1,'a');(4,'d');(2,'b');(3,'c')|]);;
-
-
-
-(* Exercise 7.2 2: *)
 
 let perm a m =
   let n = Array.length(a) in
@@ -104,19 +39,6 @@ let perm a m =
       helper a 0 m
     end;;
 
-(* Test: *)
-perm [|1|] 0;;
-perm [|1|] 1;;
-perm [|1|] 2;;
-perm [|1;2;3|] 0;;
-perm [|1;2;3|] 1;;
-perm [|1;2;3|] 2;;
-perm [|1;2;3|] 3;;
-perm [|1;2;3|] 4;;
-perm [|1;2;3|] 5;;
-perm [|1;2;3|] 6;;
-perm [|1;2;3|] 7;;
-
 let copy l1 =
   let len = Array.length l1 in
   let arr = Array.make len l1.(0) in
@@ -125,73 +47,38 @@ let copy l1 =
   done;
   arr;;
 
-let perm_test func list =
-  let len = Array.length(list) in
-  let result = ref true in
-  for i=0 to fact(len)-1 do
-    for j=i+1 to fact(len)-1 do
-      let copy1 = copy list in
-      let copy2 = copy list in
-      let noproblem = (func copy1 i != func copy2 j) in
-      if noproblem then () else Printf.printf "Problem found! %d %d\n" i j;
-      result := !result && noproblem;
-    done;
+(* Exercise 7.2 4: (Bonus) *)
+
+let remove arr index =
+  let len= Array.length arr in
+  let res = Array.make (len-1) arr.(0) in
+  for i=0 to index-1 do
+    res.(i) <- arr.(i)
   done;
-  if !result then Printf.printf "No problem found!!\n" else Printf.printf "Problem found!\n"              
-;;
+  for i=index to (len-2) do
+    res.(i) <- arr.(i+1)
+  done;
+  res;;
 
-perm_test perm [|1;2|];;
-perm_test perm [|1;2;3|];;
-perm_test perm [|1;2;3;4|];;
-perm_test perm [|1;2;3;4;5|];;
-perm_test perm [|1;2;3;4;5;6|];;
+let create_list arr =
+  let len= Array.length arr in
+  let final_arr = copy arr in
+  let help = ref (copy arr) in
+  let le = ref len in
+  for i=0 to len-1 do
+    let num = Random.int (fact(!le)-1) in
+    let per = perm !help num in
+    final_arr.(i) <- per.(0);
+    help := remove per 0;
+    le := !le - 1;
+  done;
+  final_arr;;
 
+let super_silly_sort arr =
+  let len = Array.length arr in
+  let final_arr = ref (copy arr) in
+  while is_sorted(!final_arr) do
+    final_arr := create_list arr
+  done;
+  !final_arr;;
 
-
-
-
-
-(* Exercise 7.2 3: *)
-
-module type Comparable = sig
-  type t
-  val comp : t -> t -> int
-end
-
-                       
-module StupidSorting (Comp: Comparable) = struct
-  include Comp
-
- let rec sorted ls =
-    match ls with
-    | [] -> true
-    | h :: t ->
-       List.for_all (fun e -> comp e h >= 0) t && sorted t
-
- let is_sorted arr =
-   sorted (Array.to_list arr)
-
- let silly_sort arr =
-   let all_perms = permutations arr in
-   let rec find_sorted perms =
-     match perms with
-     | [] -> raise (Failure "Something went wrong - maybe check your comparison function?")
-     | h :: t -> ( match is_sorted(h) with
-                   | true -> h
-                   | false -> find_sorted t
-                 )
-   in find_sorted all_perms
-    
-end;;          
-  
-module Desc = struct
-  type t = int
-  let comp = 
-    fun x y -> if x < y then -1
-            else if x = y then 0
-            else 1
-end;;
-
-module StupidIntDescSort = StupidSorting(Desc);;
-
-StupidIntDescSort.silly_sort [|4;2;7;1;5|];;
