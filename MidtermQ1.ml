e4748b62aa9e42a00f118e3808f3f845c6a50a47
Hebe Hilhorst <hebehh@gmail.com>
2019-03-05 19:31:46 +0800

    Fixed Question 1

--- MidtermQ1.ml
+++ MidtermQ1.ml
@@ -1,22 +1,7 @@
-open Week_01
-open Week_02
-open Week_03
-open Week_04 
-open Week_05
-
-let smallest_missing_non_negative_integer arr = 
-	let j = ref 0 in
-	while (arr.(!j)!= !j && arr.(!j)< (Array.length arr) && arr.(!j) != arr.(arr.(!j))) do
-		swap arr !j (arr.(!j));
-		j := !j + 1
-  done
-
-  for i = 0 to (Array.length arr)  do
-    if (i != arr.(i)) then i
-    else i+1  
-  done
-	
-
+let swap arr i j =
+  let tmp = arr.(i) in
+  arr.(i) <- arr.(j);
+  arr.(j) <- tmp;;
 
 
 let test_smallest_missing_non_negative_integer candidate =
@@ -29,10 +14,31 @@ let test_smallest_missing_non_negative_integer candidate =
   (candidate (Array.copy [|0; 10; 3; 33; 2; 5; 1|]) = 4) &&
   (candidate (Array.copy [|0; 10; 11; 2; 3; 7; 6; 6; 3; 33; 2; 5; 1|]) = 4) &&
   (candidate (Array.copy [|0; 10; 11; 2; 3; ~-33; 7; 6; 4; 4; 4; 6; 3; 33; 2; 5; 1|]) = 8) &&
-  (candidate (Array.copy [|1; 2; 3; 4; 5; 6; ~-33; ~-33; ~-33; 7; 8; 9; ~-33; ~-34; 12; 12; 0|]) = 10) &&
-  (candidate (Array.copy [|1; 2; 3; 4; 5; 6; 7; 8; 9|]) = 0) &&
-  (candidate (Array.copy [|10; 9; 8; 7; 0; 6; 5; 4; 3; 2; 1|]) = 11);;
+  (candidate [|1; 2; 3; 4; 5; 6; ~-33; ~-33; ~-33; 7; 8; 9; ~-33; ~-34; 12; 12; 0|] = 10) &&
+  (candidate [|1; 2; 3; 4; 5; 6; 7; 8; 9|] = 0) &&
+  (candidate [|10; 9; 8; 7; 0; 6; 5; 4; 3; 2; 1|] = 11);;
 
-  smallest_missing_non_negative_integer test_smallest_missing_non_negative_integer;;
 
+let smallest_missing_non_negative_integer arr =
+  let len = Array.length arr in
+
+  for i = 0 to len - 1 do
+    while ( i < len &&
+             arr.(i) < len &&
+               arr.(i) >= 0 &&
+                 not (i = arr.(i)) &&
+                   not (arr.(i) = arr.(arr.(i))) ) do
+      swap arr i arr.(i)
+    done
+  done;
   
+  let rec get_smallest i =
+    if i >= len then len
+    else if not(i = arr.(i)) then i
+    else get_smallest (i+1)
+  in
+  get_smallest 0;;
+                        
+
+test_smallest_missing_non_negative_integer smallest_missing_non_negative_integer;;
+
